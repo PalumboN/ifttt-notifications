42d9dac22121d84a59b7b86b11234375a7f82b44
PalumboN <nahuel.palumbo@gmail.com>
2016-08-10 07:40:53 -0300

    Autorequest for Heroku

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 require("coffee-script/register");
 require("./src/server");
+
+var rp = require('request-promise')
+var myself = process.env.MYSELF
+if (myself) {
+  setInterval(function() {
+    rp.get(myself);
+  }, 300000); // every 5 minutes (300000)
+}
